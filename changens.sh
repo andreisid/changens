253a520e20b4f098d515e702d84b3ce8bc637e7b
andrei <andrei_sid@yahoo.com>
2018-12-18 11:58:08 +0200

    started refactorization

--- changens.sh
+++ changens.sh
@@ -6,11 +6,10 @@ function showUsage {
    Usage: $progname [-i|--interface=<int_name>] [-n|--namespace=<namespace>] [-h|--h|-help|--help]
 
    optional arguments:
-     -h, --help           Show this help message and exit
      -i, --interface <int_name>        Name of the wireless interface to move to a new netwok space
      -n, --namespace <namespace>       Network namespace to move the interface to
      -c, --conf <file>                 Configuration file for logging to WPA wireless network
-
+     -h, --help           Show this help message and exit
     To run a process in a namespace do: ip netns exec <namespace> <process>
     Example: ip netns exec wifi curl ifconfig.me
              ip netns exec wifi bash; su - myuser; DISPLAY=:0 firefox  
@@ -19,65 +18,21 @@ HEREDOC
 
 # Remove interface from namespace. 
 function rollback {
-ip netns exec ${NAMESPACE} iw phy $PHY_INTERFACE set netns 1 || exit
-echo "Interface ${INTERFACE} moved out of ${NAMESPACE}"
-systemctl restart wpa_supplicant 
-echo "Restarted wpa_supplicant"
-ip netns delete wifi
-echo "Namespace ${NAMESPACE} deleted"
-exit
+    local INTERFACE=$(head -1 /tmp/.changens.cfg)
+    local PHY_INTERFACE=$(head -2 /tmp/.changens.cfg|tail -1)
+    local NAMESPACE=$(head -3 /tmp/.changens.cfg|tail -1)
+    local CONF_FILE=$(head -4 /tmp/.changens.cfg|tail -1)
+
+ip netns exec ${NAMESPACE} iw phy ${PHY_INTERFACE} set netns 1 && echo "Interface ${INTERFACE} moved out of ${NAMESPACE}"
+systemctl restart wpa_supplicant && echo "Restarted wpa_supplicant"
+ip netns delete wifi && echo "Namespace ${NAMESPACE} deleted"
 }
 
-if [ "$#" == 0 ]; then
-showUsage
-fi
-# Default values
-INTERFACE="wlan0"
-PHY_INTERFACE="phy0"
-NAMESPACE="wifi"
-CONF_FILE="./wpa_supplicant.conf"
-for i in "$@"
-do
-case $i in
-    -i=*|--interface=*)
-    INTERFACE="${i#*=}" # http://tldp.org/LDP/abs/html/string-manipulation.html  - substring removal
-    shift # past argument=value
-    ;;
-    -n=*|--namespace=*)
-    NAMESPACE="${i#*=}"
-    shift # past argument=value
-    ;;
-    -c=*|--conf=*)
-    CONF_FILE="${i#*=}"
-    shift # past argument=value
-    ;;
-    -h|--help|-help|--h)
-    showUsage
-    exit
-    ;;
-    -r|--rollback)
-    rollback
-    shift # past argument=value
-    ;;
-    
-    *)
-          # unknown option
-    ;;
-esac
-done
-
-# Remove interface from namespace. 
-function rollback {
-ip netns exec ${NAMESPACE} iw phy $(cat /sys/class/net/${INTERFACE}/phy80211/name) set netns 1 || exit
-echo "Interface ${INTERFACE} moved out of ${NAMESPACE}"
-systemctl restart wpa_supplicant 
-echo "Restarted wpa_supplicant"
-ip netns delete wifi
-echo "Namespace ${NAMESPACE} deleted"
-}
+# Create network Namespace, add interface to namespace, connect interface to Access Point
+function start {
 
 # Check if the namespace already exists, and create it if not
-exists=0
+local exists=0
 for i in $(ip netns)
 do 
 if [ "$NAMESPACE" == "$i" ]; then 
@@ -103,7 +58,6 @@ fi
 
 # If the interface specified is wireless, move it to the specified namespace, or exit the script
 # 
-export PHY_INTERFACE=$(cat /sys/class/net/${INTERFACE}/phy80211/name)
 if [ "/sys/class/net/${INTERFACE}/phy80211/name" ]; then
     iw phy $PHY_INTERFACE set netns "$(ip netns exec ${NAMESPACE} sh -c 'sleep 1 >&- & echo "$!"')"
     echo "Moved interface ${INTERFACE} to ${NAMESPACE} network namespace "
@@ -126,5 +80,71 @@ Config file for WPA AP is missing. Interface ${INTERFACE} is now moved to ${NAME
 In order to connect please run the script with a config file. Se provided config as example
 HEREDOC
 fi
+}
 
+
+# Starts the execution of the script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+readonly PROGNAME=$(basename $0)
+readonly PROGDIR=$(readlink -m $(dirname $0))
+readonly ARGS="$@"
+
+# Default values 
+INTERFACE="wlan0"
+PHY_INTERFACE="phy0"
+NAMESPACE="wifi"
+CONF_FILE="./wpa_supplicant.conf"
+
+for i in ${ARGS}
+do
+case $i in
+    -i=*|--interface=*)
+    INTERFACE="${i#*=}" # http://tldp.org/LDP/abs/html/string-manipulation.html  - substring removal
+    shift # past argument=value
+    ;;
+    -n=*|--namespace=*)
+    NAMESPACE="${i#*=}"
+    shift # past argument=value
+    ;;
+    -c=*|--conf=*)
+    CONF_FILE="${i#*=}"
+    shift # past argument=value
+    ;;
+    -h|--help|-help|--h)
+    showUsage
+    shift
+    exit
+    ;;
+    -r|--rollback)
+    rollback
+    shift # past argument=value
+    exit
+    ;;
+    
+    *)
+          # unknown option
+    ;;
+esac
+done
+
+# Starts the execution of the script
+
+# If interface provided does not exist, exit.
+is_interface=$(ip a|grep $INTERFACE)
+if [[ -z "$is_interface" ]]; then
+   echo "Provided interface does not exist" 
+   exit 1
+fi
+
+PHY_INTERFACE=$(cat /sys/class/net/${INTERFACE}/phy80211/name)
+echo $INTERFACE > /tmp/.changens.cfg
+echo $PHY_INTERFACE >> /tmp/.changens.cfg
+echo $NAMESPACE >> /tmp/.changens.cfg
+echo $CONF_FILE >> /tmp/.changens.cfg
+
+start
 
